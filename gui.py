bd57ec07f8e7d034a946c78b21a18d3a290b900a
= <=>
2020-12-11 01:08:34 +0600

    HMM implemented

--- gui.py
+++ gui.py
@@ -1,6 +1,8 @@
 import pygame
 import time
 import sys
+import numpy as np
+import random
 
 arg_count = len(sys.argv)
 if (arg_count != 3) and sys.argv[1] != "-dim":
@@ -85,21 +87,6 @@ def change_color(x, y, color):
     gameDisplay.blit(board, (0, 100))
 
 
-def show_probability(bust_X=-1, bust_Y=-1):
-    for i in range(board_dim):
-        for j in range(board_dim):
-            if i == bust_X and j == bust_Y:
-                centerY = 100 + (i * square_size)+0.5*square_size
-                centerX = (j * square_size)+0.5*square_size
-                show_text("",centerX, centerY, 20, black, "magneto")
-
-            else:
-                centerY = 100 + (i * square_size)+0.5*square_size
-                centerX = (j * square_size)+0.5*square_size
-                show_text(str(round(probability[i][j], 2)),
-                          centerX, centerY, 20, black, "magneto")
-            
-
 button_width, button_height, time_x, bust_x, button_y = 140, 50, 180, 380, 25
 pygame.draw.rect(gameDisplay, black,
                  (0, 0, display_width, 100))
@@ -111,8 +98,155 @@ show_text("TIME+1", 250, 50, 25, white)
 show_text("BUST", 450, 50, 25, white)
 pygame.display.flip()
 
+
+ghost_lottery = ["" for i in range(25)]
+for i in range(5):
+    ghost_lottery[4 * i] = "UP"
+    ghost_lottery[4 * i+1] = "DOWN"
+    ghost_lottery[4 * i+2] = "LEFT"
+    ghost_lottery[4 * i+3] = "RIGHT"
+ghost_lottery[20] = "TOP_LEFT"
+ghost_lottery[21] = "TOP_RIGHT"
+ghost_lottery[22] = "BOTTOM_LEFT"
+ghost_lottery[23] = "BOTTOM_RIGHT"
+ghost_lottery[24] = "STAY"
+random.shuffle(ghost_lottery)
+
+
+total_color = 3
+red_distance = int(board_dim / 3)
+yellow_distace = int(board_dim * .67)
+green_distance = board_dim
+color_to_distace = [red_distance, yellow_distace, green_distance]
+
+#initialize values
 init_prob = (1 / (board_dim * board_dim))
-probability = [[init_prob for i in range(board_dim)] for j in range(board_dim)]
+probability = np.full((1,board_dim*board_dim), init_prob)
+
+
+
+def get_transitional_probability(indexA, indexB):
+    transition_distribution = {"STRAIGHT": 0.2,
+                               "DIAGONAL": 0.04, "STAY": 0.04}
+    indexA_col = indexA % board_dim
+    indexA_row = (indexA - indexA_col) // board_dim
+
+    indexB_col = indexB % board_dim
+    indexB_row = (indexB - indexB_col) // board_dim
+
+    if indexA == indexB:
+        return transition_distribution["STAY"]
+    elif abs(indexA_row - indexB_row)== 1 and (indexA_col == indexB_col):
+        return transition_distribution["STRAIGHT"]
+    elif abs(indexA_col - indexB_col)== 1 and (indexA_row == indexB_row):
+        return transition_distribution["STRAIGHT"]
+    elif abs(indexA_row - indexB_row) == 1 and abs(indexA_col - indexB_col) == 1:
+        return transition_distribution["DIAGONAL"]
+    else:
+        return 0
+
+def show_probability(bust_X=-1, bust_Y=-1):
+    for i in range(board_dim):
+        for j in range(board_dim):
+            if i == bust_X and j == bust_Y:
+                centerY = 100 + (i * square_size)+0.5*square_size
+                centerX = (j * square_size)+0.5*square_size
+                show_text("", centerX, centerY, 20, black, "magneto")
+
+            else:
+                centerY = 100 + (i * square_size)+0.5*square_size
+                centerX = (j * square_size)+0.5*square_size
+                show_text(str(np.round(probability[0, i*board_dim+j],2)),
+                          centerX, centerY, 20, black, "magneto")
+
+
+def move_ghost(prev_x, prev_y):
+    move_taken = False
+    
+    while not move_taken:
+        move = random.randint(0, 24)
+        move = ghost_lottery[move]
+        # print(f"cur_x: {prev_x} cur_y: {prev_y} move: {move}")
+        if move == "STAY":
+            cur_x = prev_x
+            cur_y = prev_y
+            move_taken = True
+        elif move == "UP":
+            cur_x = prev_x - 1
+            cur_y = prev_y
+            move_taken = True if cur_x >= 0 else False
+        elif move == "DOWN":
+            cur_x = prev_x + 1
+            cur_y = prev_y
+            move_taken = True if cur_x < board_dim else False
+        elif move == "LEFT":
+            cur_x = prev_x
+            cur_y = prev_y - 1
+            move_taken = True if cur_y >= 0 else False
+        elif move == "RIGHT":
+            cur_x = prev_x
+            cur_y = prev_y + 1
+            move_taken = True if cur_y < board_dim else False
+        elif move == "TOP_LEFT":
+            cur_x = prev_x - 1
+            cur_y = prev_y - 1
+            move_taken = True if (cur_x >= 0 and cur_y >= 0) else False
+        elif move == "TOP_RIGHT":
+            cur_x = prev_x - 1
+            cur_y = prev_y + 1
+            move_taken = True if (cur_x >= 0 and cur_y < board_dim) else False
+        elif move == "BOTTOM_LEFT":
+            cur_x = prev_x + 1
+            cur_y = prev_y - 1
+            move_taken = True if (cur_x < board_dim and cur_y >= 0) else False
+        elif move == "BOTTOM_RIGHT":
+            cur_x = prev_x + 1
+            cur_y = prev_y + 1
+            move_taken = True if (cur_x < board_dim and cur_y < board_dim) else False
+    return (cur_x,cur_y)
+
+def get_emission_probability(indexA, indexB):
+    indexA_col = indexA % board_dim
+    indexA_row = (indexA - indexA_col) // board_dim
+
+    color = indexB % total_color  #(0-->red,1-->yellow,2-->green)
+    indexB = (indexB - color) // total_color
+    indexB_col = indexB % board_dim
+    indexB_row = (indexB - indexB_col) // board_dim
+
+    man_dis = abs(indexA_row - indexB_row) + abs(indexA_col - indexB_col)
+ 
+    if color == 0:
+        return 1 if man_dis <= red_distance else 0
+    elif color == 1:
+        return 1 if (man_dis > red_distance and man_dis <= yellow_distace) else 0
+    else:
+        return 1 if man_dis > yellow_distace else 0
+
+
+
+transition_Matrix = np.zeros((board_dim*board_dim, board_dim*board_dim))
+for i in range(board_dim*board_dim):
+    for j in range(board_dim*board_dim):
+        transition_Matrix[i][j] = get_transitional_probability(i, j)
+
+#Normalize Transition Matrix
+norm = np.linalg.norm(transition_Matrix, ord=1, axis=1, keepdims=True) + 1e-9
+transition_Matrix = transition_Matrix/norm
+
+#Emission Matrix
+print(color_to_distace)
+emission_Matrix = np.zeros((board_dim*board_dim,board_dim * board_dim * total_color))
+for i in range(board_dim*board_dim):
+    for j in range(board_dim*board_dim*total_color):
+        emission_Matrix[i][j] = get_emission_probability(i, j)
+
+#Normalize Emission Matrix
+norm = np.linalg.norm(emission_Matrix, ord=1, axis=1, keepdims=True) + 1e-9
+emission_Matrix = emission_Matrix / norm
+
+ghost_x = random.randint(0,board_dim-1)
+ghost_y = random.randint(0, board_dim - 1)
 
 draw_board()
 show_probability()
@@ -125,22 +259,31 @@ while not game_quit:
     if bust_clicked:
         centerX = (last_Y * square_size)+0.5*square_size
         centerY = 100 + (last_X * square_size) + 0.5 * square_size
-        show_probability(last_X,last_Y)
+        show_probability(last_X, last_Y)
         show_text(bust_text, centerX, centerY, 18, black, "magneto")
         pygame.display.flip()
 
     for event in pygame.event.get():
         if event.type == pygame.MOUSEBUTTONUP and not bust_clicked:
             posX, posY = pygame.mouse.get_pos()
+            # print(ghost_x, ghost_y)
 
             if posY < 100:
                 if posX >= time_x and posX <= (time_x + button_width) and posY >= button_y and posY <= (button_y + button_height):
-                    probability[last_X][last_Y] = (last_X+last_Y)/100
+                    # probability[last_X*board_dim+last_Y] = last_X+last_Y
+                    probability = np.matmul(probability, transition_Matrix)
+                    norm = np.linalg.norm(probability, ord=1, axis=1, keepdims=True) + 1e-9
+                    probability = probability / norm
+                    ghost_x, ghost_y = move_ghost(ghost_x,ghost_y)
                 elif posX >= bust_x and posX <= (bust_x + button_width) and posY >= button_y and posY <= (button_y + button_height):
                     change_color(last_X, last_Y, blue)
+                    if ghost_x == last_X and ghost_y == last_Y:
+                        bust_text = "HIT"
+                    else:
+                        bust_text = "MISS"
+                    print(f"ghost location: {ghost_x} {ghost_y}")
                     bust_clicked = True
-                    bust_text = "HIT"
-                
+
                 show_probability()
                 pygame.display.flip()
 
@@ -148,8 +291,26 @@ while not game_quit:
                 posY = posY - 100
                 p_j = int((posX - 0.001)) // square_size
                 p_i = int((posY - 0.001)) // square_size
-                last_X,last_Y = int(p_i),int(p_j)
-                change_color(p_i, p_j, red)
+                last_X, last_Y = int(p_i), int(p_j)
+                manhattan_distance = abs(p_i - ghost_x) + abs(p_j - ghost_y)
+                if manhattan_distance <= red_distance:
+                    color = red
+                    color_index = 0
+                elif manhattan_distance <= yellow_distace:
+                    color = yellow
+                    color_index = 1
+                else:
+                    color = green
+                    color_index = 2
+
+                change_color(p_i, p_j, color)
+                emission_col = int((((board_dim * p_i) + p_j) * total_color) + color_index)
+                emission_prob = emission_Matrix[:, emission_col]
+
+                probability = (probability * emission_prob) / np.sum(emission_prob)
+                norm = np.linalg.norm(probability, ord=1, axis=1, keepdims=True) + 1e-9
+                probability = probability / norm
+
                 show_probability()
                 pygame.display.flip()
 
@@ -157,5 +318,4 @@ while not game_quit:
             pygame.quit()
             quit()
 
-    
     clock.tick(60)
